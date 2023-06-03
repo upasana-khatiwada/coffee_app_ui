61c00d348be6e9af1f837945208bb3291ef1c53b
upasana khatiwada <upashanakhatiwada1234@gmail.com>
2023-06-03 20:55:19 +0545

    Search bar

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -11,25 +11,57 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leading: const Icon(Icons.menu),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.only(right: 8),
+            child: Icon(Icons.person),
+          ),
+        ],
+      ),
       bottomNavigationBar: BottomNavigationBar(items: const [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          label: ''
-        ),
-         BottomNavigationBarItem(
-          icon: Icon(Icons.shopping_bag),
-          label: ''
-        ),
-         BottomNavigationBarItem(
-          icon: Icon(Icons.favorite),
-          label: ''
-        ),
-         BottomNavigationBarItem(
-          icon: Icon(Icons.notifications),
-          label: ''
-        ),
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+        BottomNavigationBarItem(icon: Icon(Icons.shopping_bag), label: ''),
+        BottomNavigationBarItem(icon: Icon(Icons.favorite), label: ''),
+        BottomNavigationBarItem(icon: Icon(Icons.notifications), label: ''),
       ]),
+      body: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(15.0),
+            child: Text(
+              "Find the best coffee for you",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+
+          const SizedBox(
+            height: 20,
+          ),
+
+          //search Bar
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: TextField(
+              decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.search),
+                  hintText: 'Find your coffee',
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey.shade600),
+                      ),
+                      enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey.shade600),),
+                      ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
